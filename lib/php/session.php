5ecf260ad3473c39ee88356d5af18fc10da25883
Alejandro U. Alvarez <alejandro@urbanoalvarez.es>
2012-11-20 17:02:10 +0100

    All required files up
    Added session.php and improved .gitignore

---
+++ lib/php/session.php
@@ -0,0 +1,99 @@
+<?php
+error_reporting(E_ALL);
+ini_set('error_reporting', E_ALL);
+session_start();
+/*	Session handler, must be included anywhere you want to track users
+ *  it also handles all includes and such. So it's the only file that
+ *  is really needed.
+ *
+ *	By Alejandro U. Alvarez (http://urbanoalvarez.es)
+*/
+include('locale.php');		// Local settings
+include('db.class.php');	// MySQL abstraction layer
+include('dropbox.php');		// Dropbox API layer
+
+class Session{
+	private $logged = false;
+	private $usid;
+	private $db = false;	// Database connection holder
+	
+	public function Session(){
+		// Constructor, no params
+		if($_SESSION['user']['logged']){
+			$this->usid = $_SESSION['user']['id'];
+			$this->logged = true;
+		}/*else{
+			// Rebuild session
+			$_SESSION = array(
+				'dropbox'=>array(
+					'oauth_tokens'=>array(
+						'token'=>'if4o5njtgmt9etz',
+						'token_secret'=>'e906am88oypu67d',
+						'state'=>1,
+						'id'=>57833018
+						,'email'=>'aluralma@gmail.com',
+						'root'=>1
+					)
+				),
+				'logged'=>1,
+				'id'=>1
+			);
+			$this->usid = $_SESSION['user']['id'];
+			$this->logged = true;	
+		}*/
+	}
+	
+	// Database connection
+	public function db(){
+		if($this->db) return $this->db;
+		// Load private data file
+		include('db.ignore.php');
+		return $this->db = new DB($database,$host,$user,$pass);	
+	}
+	
+	// Returns true if user logged in
+	public function logged(){
+		return $this->logged;
+	}
+	
+	public function login($service,$id){
+		// Setup login for the given service and id
+		// Also pull the necessary variables from DB
+		// like Cloudclient ID
+		switch($service){
+			case 'cloudclient':
+				// Our standard login system
+				$usid = $id;
+				break;
+			case 'dropbox':
+				// Dropbox login
+				// Fetch user id, if there is any. If not, create a new one.
+				$db = $this->db();
+				$usid = $db->preparedQuery('SELECT id FROM users WHERE dropbox = ? LIMIT 1',array($id));
+				$usid = $db->fetchNext($usid);
+				if($usid && $usid>0){
+					// User already existed
+				}else{
+					// Users doesn't exist, insert new
+					$usid = $db->preparedInsert('users',array('dropbox'=>$id,'jtime'=>time()));	
+				}
+				// We now have the usid, create session vars
+				$this->logged = $_SESSION['user']['logged'] = true;
+				$_SESSION['user']['id'] = $usid;
+				break;
+		}
+	}
+	
+	public function usid(){
+		return $this->usid;	
+	}
+	
+	public function loginLink(){
+		$loginURI = 'https://www.dropbox.com/1/oauth/authorize';
+		$tokenURI = 'https://api.dropbox.com/1/oauth/access_token';
+		$token = $this->getToken();
+        return $loginURI . '?oauth_token=' . $token['token'] . '&oauth_callback=local.cloudclient.es/dpAuth.php';
+	}
+};
+$sess = new Session();
+$dp = new Dropbox();
